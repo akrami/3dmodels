6efbea6b8942aecb449b30902f356660be101d2b
Alireza Akrami <2144764+akrami@users.noreply.github.com>
2025-06-22 21:44:49 +0200

    Refine cylinder insert geometry

--- src/models/waveplanter/insert.tsx
+++ src/models/waveplanter/insert.tsx
@@ -1,31 +1,32 @@
 import * as React from "react";
 import * as THREE from "three";
-import { mergeGeometries, mergeVertices as mergeVerts } from "three/examples/jsm/utils/BufferGeometryUtils.js";
+import {
+  mergeGeometries,
+  mergeVertices as mergeVerts,
+} from "three/examples/jsm/utils/BufferGeometryUtils.js";
 import { Brush, Evaluator, SUBTRACTION } from "three-bvh-csg";
-import { circle, extrude } from "./ringGear";
 import type { WavePlanterProps } from "./props";
 
 export default function InsertObject({ props, color }: { props: WavePlanterProps; color: string }) {
   const geometry = React.useMemo(() => {
     const height = Math.max(props.baseDepth - 12, 0);
 
-    const ringShape = circle(10);
-    ringShape.holes.push(new THREE.Path().absarc(0, 0, 8, 0, Math.PI * 2, true));
-    const ringGeom = extrude(ringShape, 2);
-    ringGeom.translate(0, 0, height + 2);
+    const ringGeom = new THREE.CylinderGeometry(10, 10, 2, 16, 1, true);
+    ringGeom.translate(0, 0, height + 3);
 
-    const cylGeom = extrude(circle(10), height);
-    cylGeom.translate(0, 0, 2);
+    const cylGeom = new THREE.CylinderGeometry(10, 10, height, 16, 1, true);
+    cylGeom.translate(0, 0, 2 + height / 2);
 
-    const diskGeom = extrude(circle(8), 2);
+    const diskGeom = new THREE.CylinderGeometry(8, 8, 2, 16);
+    diskGeom.translate(0, 0, 1);
 
     const outer = mergeGeometries([diskGeom, cylGeom, ringGeom], false)!;
 
     const evaluator = new Evaluator();
     evaluator.useGroups = false;
 
-    const innerGeom = extrude(circle(8), height + 2);
-    innerGeom.translate(0, 0, 2);
+    const innerGeom = new THREE.CylinderGeometry(8, 8, height + 2, 16, 1, true);
+    innerGeom.translate(0, 0, 3 + height / 2);
     let result = evaluator.evaluate(
       new Brush(outer),
       new Brush(innerGeom),
@@ -40,6 +41,7 @@ export default function InsertObject({ props, color }: { props: WavePlanterProps
     const angleStep = (2 * Math.PI) / aroundCount;
     const verticalCount = Math.max(1, Math.floor(height / holeSpacing));
 
+    const holeGeoms: THREE.BufferGeometry[] = [];
     for (let i = 0; i < verticalCount; i++) {
       const z = 2 + holeSpacing / 2 + i * holeSpacing;
       const offset = (i % 2) * angleStep * 0.5;
@@ -47,13 +49,15 @@ export default function InsertObject({ props, color }: { props: WavePlanterProps
         const angle = j * angleStep + offset;
         const x = Math.cos(angle) * radial;
         const y = Math.sin(angle) * radial;
-        const hole = new Brush(new THREE.CylinderGeometry(holeRadius, holeRadius, 20, 32));
-        hole.position.set(x, y, z);
-        hole.rotation.set(0, 0, angle);
-        hole.rotateX(Math.PI / 2);
-        hole.updateMatrixWorld();
-        result = evaluator.evaluate(result, hole, SUBTRACTION) as THREE.Mesh;
+        const g = new THREE.CylinderGeometry(holeRadius, holeRadius, 4, 8);
+        g.rotateX(Math.PI / 2);
+        g.translate(x, y, z);
+        holeGeoms.push(g);
       }
+    }
+    if (holeGeoms.length > 0) {
+      const holes = mergeGeometries(holeGeoms, false)!;
+      result = evaluator.evaluate(result, new Brush(holes), SUBTRACTION) as THREE.Mesh;
     }
 
     const slotLength = 12;
@@ -67,7 +71,12 @@ export default function InsertObject({ props, color }: { props: WavePlanterProps
       .lineTo(-halfLen, -slotRadius)
       .absarc(-halfLen, 0, slotRadius, -Math.PI / 2, Math.PI / 2, false);
     slotShape.closePath();
-    const slotGeom = extrude(slotShape, 2);
+    const slotGeom = new THREE.ExtrudeGeometry(slotShape, {
+      depth: 2,
+      steps: 1,
+      bevelEnabled: false,
+      curveSegments: 16,
+    });
 
     for (let i = 0; i < 3; i++) {
       const angle = (i / 3) * Math.PI * 2;
