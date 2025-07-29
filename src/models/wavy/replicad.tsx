789dfdc573a7a4ee59f3e175aedc21dc5d639374
Alireza Akrami <2144764+akrami@users.noreply.github.com>
2025-07-29 15:26:40 +0200

    Fix R3F hook usage

--- src/models/wavy/replicad.tsx
+++ src/models/wavy/replicad.tsx
@@ -8,47 +8,60 @@ import { BufferGeometry } from "three";
 import { syncFaces } from "replicad-threejs-helper/dist/es/replicad-threejs-helper";
 import { getGlobalMaterial } from "@/utils/properties";
 
-export default function WavyReplicad() {
-    const { invalidate } = useThree();
-    const [mesh, setMesh] = useState(null);
-    useEffect(() => {
-        createMesh().then((m) => setMesh(m));
-    }, []);
-    const body = useRef(new BufferGeometry());
-    const faces = mesh.faces;
-    useLayoutEffect(() => {
-        if (faces) syncFaces(body.current, faces);
-    }, [faces, invalidate]);
-    useEffect(
-        () => () => {
-            body.current.dispose();
-            invalidate();
-        },
-        [invalidate]
-    );
+function Scene({ faces }: { faces: any }) {
+  const { invalidate } = useThree();
+  const body = useRef(new BufferGeometry());
 
-    return (
-        <AppLayout>
-            <SidebarProvider>
-                <div className="flex flex-1">
-                    <div className="flex-1 relative">
-                        <Canvas
-                            camera={{ position: [200, 200, 200], fov: 60, up: [0, 1, 0] }}
-                            className="bg-gradient-to-br from-gray-50 to-gray-200"
-                            shadows
-                        >
-                            <ambientLight intensity={0.6} />
-                            <directionalLight position={[50, 100, 70]} intensity={1} castShadow />
-                            <gridHelper args={[500, 50, "#888888", "#444444"]} />
-                            <axesHelper args={[500]} />
-                            <group>
-                                <mesh geometry={body.current} material={getGlobalMaterial("#A7C7E7")}/>
-                            </group>
-                            <OrbitControls />
-                        </Canvas>
-                    </div>
-                </div>
-            </SidebarProvider>
-        </AppLayout>
-    )
+  useLayoutEffect(() => {
+    if (faces) syncFaces(body.current, faces);
+  }, [faces, invalidate]);
+
+  useEffect(
+    () => () => {
+      body.current.dispose();
+      invalidate();
+    },
+    [invalidate]
+  );
+
+  return (
+    <>
+      <ambientLight intensity={0.6} />
+      <directionalLight position={[50, 100, 70]} intensity={1} castShadow />
+      <gridHelper args={[500, 50, "#888888", "#444444"]} />
+      <axesHelper args={[500]} />
+      <group>
+        <mesh geometry={body.current} material={getGlobalMaterial("#A7C7E7")} />
+      </group>
+      <OrbitControls />
+    </>
+  );
+}
+
+export default function WavyReplicad() {
+  const [mesh, setMesh] = useState<any>(null);
+
+  useEffect(() => {
+    createMesh().then((m) => setMesh(m));
+  }, []);
+
+  const faces = mesh?.faces;
+
+  return (
+    <AppLayout>
+      <SidebarProvider>
+        <div className="flex flex-1">
+          <div className="flex-1 relative">
+            <Canvas
+              camera={{ position: [200, 200, 200], fov: 60, up: [0, 1, 0] }}
+              className="bg-gradient-to-br from-gray-50 to-gray-200"
+              shadows
+            >
+              <Scene faces={faces} />
+            </Canvas>
+          </div>
+        </div>
+      </SidebarProvider>
+    </AppLayout>
+  );
 }
